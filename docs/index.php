a12487a3d91ccd96c97ca9ddb9b5cf32a62e92be
Ghosty786 <bleedmenomercy786@gmail.com>
2022-09-12 15:27:51 +0100

    new file:   README.md 	modified:   docs/index.php 	modified:   includes/footer.php 	modified:   includes/global.php 	modified:   includes/head.php 	modified:   includes/header.php 	modified:   includes/navbar.php 	modified:   index.php 	modified:   package-lock.json 	modified:   package.json 	modified:   text.css 	modified:   ui/css/style.css 	modified:   ui/js/code.js

--- docs/index.php
+++ docs/index.php
@@ -5,11 +5,11 @@
     <!--====== Title ======-->
     <title> Mirzapur Roleplay Docs | Home</title>
     <?php include('../includes/head.php') ?>
+    
 </head>
 <body>
     
     <?php include('../includes/navbar.php') ?>
-    <?php include('../includes/header.php') ?>
      <div class="container py-5 text-center">
         <h1 class="py-5">MZRP DOCS</h1>   
         <div class="row">
@@ -18,7 +18,7 @@
                     <div class="d-flex align-items-center justify-content-center" style="min-height:300px">
                         <div> 
                             <i class="bi bi-person display-1"></i>
-                            <h3>Run By The Civilians</h3>
+                            <h3>Run By The Civilians 2</h3>
                             <p>
                                 Everything in the city is run by civilians apart from some local doctors at Pillbox. Hit up Twitter to find the right employee who can help you!
                             </p>
--- includes/head.php
+++ includes/head.php
@@ -1,7 +1,9 @@
     <?php include('global.php') ?>
+
     <!--====== Favicon Icon ======-->
     <link rel="shortcut icon" href="ui/img/favicon.ico" type="image/png">
     <!--====== Favicon Icon ======-->
+    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="<?php echo $URL ?>/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
